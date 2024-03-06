ae42f2ebd6cafdab7307e5d24441c8a89f263b1d
sof.en9ineer <sof.en9ineer@gmail.com>
2024-03-06 21:47:11 +0000

    Remove spaces

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymajps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
---
 vim.keymap.set("n", "<leader>s", ":source $HOME/.config/nvim/init.lua <CR>")
 vim.keymap.set("n", "<leader>sc", ":source % <CR>")
 
